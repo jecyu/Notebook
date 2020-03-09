71eabc391de9d5df40d8e3db5f7f0e3ed9c0561f
Jecyu <jecyu.lin@gmail.com>
2020-03-09 09:22:19 +0800

    📝2020-03-09 09:22:19

--- examples/cs-network/TCPClient.py
+++ examples/cs-network/TCPClient.py
@@ -1,6 +1,6 @@
 from socket import *
-serverName = 'servername'
-serverPort = 12000
+serverName = 'servername' # 服务器的名字
+serverPort = 12000 # 服务器上的进程
 clientSocket = socket(AF_INET, SOCK_STREAM) # 创建客户套接字 SOCK_STREAM 表面是 TCP 套接字
 clientSocket.connect(serverName, serverPort) # 进行 TCP 连接
 sentence = raw_input('Input lowercase sentence:') # 字符串 sentence 连续收集字符直到用户键入回撤以终止该行为为止
