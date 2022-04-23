0fb65c2039f01e279683fb627d6a05921d195768
Jecyu <jecyu.lin@gmail.com>
2022-04-23 23:01:48 +0800

    feat: 添加 servlet-hello-world

---
+++ examples/37-java/servlet-hello-world/src/main/java/HelloServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// WebServlet注解表示这是一个Servlet，并映射到地址/:
+@WebServlet(urlPatterns = "/")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // 设置响应类型:
+        resp.setContentType("text/html");
+        // 获取输出流:
+        PrintWriter pw = resp.getWriter();
+        // 写入响应:
+        pw.write("<h1>Hello, naluduo!</h1>");
+        // 最后不要忘记flush强制输出:
+        pw.flush();
+    }
+}
